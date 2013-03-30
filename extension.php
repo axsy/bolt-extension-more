370f2038728754045d8c3ca0dcd8b2cb735e7a76
Aleksey Orlov <i.trancer@gmail.com>
2013-03-31 01:30:43 +0200

    Implemented more, wrote readme

--- extension.php
+++ extension.php
@@ -7,34 +7,44 @@
  * file that was distributed with this source code.
  */
 
-namespace More
+namespace More;
+
+use Bolt\BaseExtension;
+use Bolt\Content;
+
+class Extension extends BaseExtension
 {
-    use Bolt\BaseExtension;
-
-    class Extension extends BaseExtension
+    function info()
     {
-        function info()
-        {
-            return array(
-                'name' => "More",
-                'description' => "An extension provides capability to break the post into teaser and content sections",
-                'keywords' => "bolt, extension, more",
-                'author' => "Aleksey Orlov",
-                'link' => "https://github.com/axsy/bolt-extension-more",
-                'version' => "0.1",
-                'required_bolt_version' => "1.0.2",
-                'highest_bolt_version' => "1.0.2",
-                'type' => "General",
-                'first_releasedate' => "2013-03-30",
-                'latest_releasedate' => "2013-03-30",
-                'dependencies' => "",
-                'priority' => 10
-            );
-        }
+        return array(
+            'name' => "More",
+            'description' => "An extension provides capability to break the post into teaser and content sections",
+            'keywords' => "bolt, extension, more",
+            'author' => "Aleksey Orlov",
+            'link' => "https://github.com/axsy/bolt-extension-more",
+            'version' => "0.1",
+            'required_bolt_version' => "1.0.2",
+            'highest_bolt_version' => "1.0.2",
+            'type' => "General",
+            'first_releasedate' => "2013-03-30",
+            'latest_releasedate' => "2013-03-30",
+            'dependencies' => "",
+            'priority' => 10
+        );
+    }
 
-        function init($app)
-        {
-        }
+    function initialize()
+    {
+        $this->app['twig']->addFilter('more', new \Twig_SimpleFilter('more', function (Content $content, $field) {
+            if (preg_match('/^(.*?)\{#\s*more\s*#\}/ms', $content->values[$field], $matches)) {
+                $content = clone $content;
+                $content->values[$field] = $matches[1];
+            }
+
+            return $content->offsetGet($field);
+        }, array(
+            'is_safe' => array('html')
+        )));
     }
 
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,58 @@
 More
 ====
 
-An extension provides capability to break the post into teaser and content sections
+An extension provides capability to break the post into teaser and content sections. You can read more about
+awesome Bolt CMS built on top of Silex microframework at [Bolt.cm](http://bolt.cm).
+
+Usage
+-----
+
+All you have to do to make your post supports 'more' feature is to insert `{# more #}` to any place in your post,
+change the index loop and add `{{ record|more('<field_name>') }}` to the listing templates (instead of usual `{{ record.excerpt(...) }}`.
+
+Example
+-------
+
+Assume for the simplicity that we have listing.twig only, one content type and the very basic templates:
+
+The post (markdown):
+
+    Hello, this is my awesome post!
+    {# more #}
+
+    Hello, More!
+
+listing.twig:
+
+    {% extends 'layout.twig' %}
+
+    {% block content %}
+        {% include '_records.twig' %}
+        {{ pager() }}
+    {% endblock content %}
+
+_records.twig
+
+    {% for article in records %}
+        <article>
+            <h1 class="title"><a href="{{ article.link }}">{{ article.title }} &rarr;</a></h1>
+            {{ article|more('content') }}
+        </article>
+    {% endfor %}
+
+These will output
+
+    <p>
+        Hello, this is my awesome post!
+    </p>
+
+on the listing page, and
+
+    <p>
+        Hello, this is my awesome post!
+    </p>
+    <p>
+        Hello, More!
+    </p>
+
+on the record page
